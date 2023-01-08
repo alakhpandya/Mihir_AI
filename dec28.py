a94d6a99adb1050c04937658df06c3a90091674e
alakhpandya <alakhpandya@gmail.com>
2023-01-08 16:55:46 +0530

    create jan8

--- dec28.py
+++ dec28.py
@@ -37,44 +37,46 @@ def graph_input():
                 master_list.append(child_nodes)
                 graph[child] = child_nodes
 
-"""
+# def bfs(node):
+#     queue = []
+#     opened = []
+#     queue.append(node)
+#     for node_to_expand in queue:
+#         for child in graph[node_to_expand]:
+#             queue.append(child)
+#             # print("\n\nqueue:", queue)
+#             # print("opened:", opened)
+#         # queue.pop(0)
+#         opened.append(node_to_expand)
+#         # print("\n\nqueue:", queue)
+    # print("opened:", opened)
+
 def bfs(node):
+        global queue, opened
+        queue.append(node)
+        while queue:
+            temp = queue.pop(0)
+            print(temp, end=" ")
+            for child in graph[temp]:
+                if child not in opened:
+                    opened.append(child)
+                    queue.append(child)
+
+
+if __name__ == '__main__':
     queue = []
     opened = []
-    queue.append(node)
-    for node_to_expand in queue:
-        for child in graph[node_to_expand]:
-            queue.append(child)
-            # print("\n\nqueue:", queue)
-            # print("opened:", opened)
-        # queue.pop(0)
-        opened.append(node_to_expand)
-        # print("\n\nqueue:", queue)
-    print("opened:", opened)
-"""
-queue = []
-opened = []
-def bfs(node):
-    global queue, opened
-    queue.append(node)
-    while queue:
-        temp = queue.pop(0)
-        print(temp, end=" ")
-        for child in graph[temp]:
-            if child not in opened:
-                opened.append(child)
-                queue.append(child)
+    
+    # Main
+    # graph_input()
+    # print(graph)
+    bfs('a')
 
-# Main
-# graph_input()
-# print(graph)
-bfs('a')
+    # test = [11,21,13,41,51,16]
+    # test.pop(0)
+    # print(test)
 
-# test = [11,21,13,41,51,16]
-# test.pop(0)
-# print(test)
+    """
+    dfs o/p: a b1 c1 d1 d2 b2 b3 c2 d3 c3 d4 d5 c4
 
-"""
-dfs o/p: a b1 c1 d1 d2 b2 b3 c2 d3 c3 d4 d5 c4
-
-"""
+    """
---
+++ jan8.py
@@ -0,0 +1,14 @@
+from dec28 import graph
+
+opened = []
+def dfs(node):
+    global opened
+    if node not in opened:
+        print(node, end=" ")
+        opened.append(node)
+
+        for child in graph[node]:
+            dfs(child)
+
+# main
+dfs('a')
